400ade185d6d227184f484a8c6ca83fe66e5b555
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-14 22:06:06 +0000

    cfooter modificado

--- src/js/component/footerComponent/Footer.js
+++ src/js/component/footerComponent/Footer.js
@@ -1,16 +1,16 @@
 import React from "react";
 import './style.css';
-
+const data={
+    footer:" Copyright-2022"
+}
 const Footer = () => {
     return (
 
-        <footer className="footer text-center">
+        <footer className="footer text-center my-5">
             <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-12 footer-col">
-                        <spam>Copyright</spam>
-                    </div>
-                </div>
+                    <div className="col-md-12 footer-col my-5">
+                        {data.footer}
+                    </div>    
             </div>
         </footer>
     )
